74b7ea003555665ec651a56558a8046e740846f1
VEfimov <V.Efimov**.ru>
2020-09-15 13:21:27 +0300

    add animal name to HW3/part2/Veterinar

--- src/Homework3/part2_Animal/Veterinar.java
+++ src/Homework3/part2_Animal/Veterinar.java
@@ -3,7 +3,7 @@ package Homework3.part2_Animal;
 public class Veterinar {
 
     public void treatAnimal(Animal animal) {
-        System.out.println("\nЭто животное ест: " + animal.getFood() + " и живёт в " + animal.getLocation());
+        System.out.println("\nЭто животное (" +  animal.getClass().getSimpleName() + ") ест: " + animal.getFood() + " и живёт в " + animal.getLocation());
     }
 
 }
