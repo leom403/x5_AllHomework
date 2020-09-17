f31c9344546744bc6f7fa25f633444af47fc58b1
LeoMaven <Neo226@yandex.ru>
2020-09-18 01:20:33 +0300

    add branch hw3p2-3, WOB3 - test with additional horse

--- src/Homework3/part2_Animal/Main.java
+++ src/Homework3/part2_Animal/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 //Массив живности
-        Animal[] animal = new Animal[3];
+        Animal[] animal = new Animal[4];
         animal[0] = new Cat("Meow", "Small", true, "fish", "Home");
         animal[1] = new Dog("Bark! Bark!", "Middle", "Bpnes", "Dog pound");
         animal[2] = new Horse("I-go-go", "Tall",4,true, "Oast", "Preries" );
+        animal[3] = new Horse("I-go-go", "Tall",4,true, "Carrot", "Stable" );
 
         System.out.println(animal[0]);
         for (int i = 0; i < animal.length; i++) {
